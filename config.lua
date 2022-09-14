ee68d64ad77fee145595e4a5dd36db1848ecde40
CodexisPhantom <85877781+CodexisPhantom@users.noreply.github.com>
2022-09-14 10:31:07 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,9 +6,9 @@ QBConfig.UpdateInterval = 5 -- how often to update player data in minutes
 QBConfig.StatusInterval = 2500 -- how often to check hunger/thirst status in milliseconds
 
 QBConfig.Money = {}
-QBConfig.Money.MoneyTypes = {['cash'] = 5000, ['bank'] = 20000, ['crypto'] = 0} -- ['type'] = startamount - Add or remove money types for your server (for ex. ['blackmoney'] = 0), remember once added it will not be removed from the database!
-QBConfig.Money.DontAllowMinus = {'cash', 'crypto'} -- Money that is not allowed going in minus
-QBConfig.Money.PayCheckTimeOut = 20 -- The time in minutes that it will give the paycheck
+QBConfig.Money.MoneyTypes = { ['cash'] = 5000, ['bank'] = 20000, ['crypto'] = 0 } -- ['type'] = startamount - Add or remove money types for your server (for ex. ['blackmoney'] = 0), remember once added it will not be removed from the database!
+QBConfig.Money.DontAllowMinus = { 'cash', 'crypto' } -- Money that is not allowed going in minus
+QBConfig.Money.PayCheckTimeOut = 30 -- The time in minutes that it will give the paycheck
 QBConfig.Money.PayCheckSociety = false -- If true paycheck will come from the society account that the player is employed at, requires qb-management
 
 QBConfig.Player = {}
@@ -28,9 +28,9 @@ QBConfig.Server.Uptime = 0 -- Time the server has been up.
 QBConfig.Server.Whitelist = false -- Enable or disable whitelist on the server
 QBConfig.Server.WhitelistPermission = 'admin' -- Permission that's able to enter the server when the whitelist is on
 QBConfig.Server.PVP = true -- Enable or disable pvp on the server (Ability to shoot other players)
-QBConfig.Server.Discord = "" -- Discord invite link
+QBConfig.Server.Discord = "https://discord.vdream-rp.com/" -- Discord invite link
 QBConfig.Server.CheckDuplicateLicense = true -- Check for duplicate rockstar license on join
-QBConfig.Server.Permissions = {'god', 'admin', 'mod'} -- Add as many groups as you want here after creating them in your server.cfg
+QBConfig.Server.Permissions = { 'god', 'admin', 'mod' } -- Add as many groups as you want here after creating them in your server.cfg
 
 QBConfig.Notify = {}
 
@@ -65,4 +65,3 @@ QBConfig.Notify.VariantDefinitions = {
         icon = 'fas fa-ambulance'
     }
 }
-
